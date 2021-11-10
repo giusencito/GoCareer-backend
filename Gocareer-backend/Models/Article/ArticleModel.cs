9da85eb33ba9ef728db7c08fb3eb462291d164c1
Alejandro Huaman <U201910144@upc.edu.pe>
2021-11-10 18:55:01 -0500

    feat: final update controllers v2. Alejandro Huamán

--- Gocareer-backend/Models/Article/ArticleModel.cs
+++ Gocareer-backend/Models/Article/ArticleModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Gocareer_backend.Models
+namespace Gocareer_backend.Models.Article
 {
     public class ArticleModel
     {
